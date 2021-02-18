a211c75332890d26341c90cc2983aabfef2e421a
Manu <3916435+m3nu@users.noreply.github.com>
2021-02-18 17:45:32 +0800

    Remove hard dependency on outdated osxfuse formula

--- Formula/borgbackup-fuse.rb
+++ Formula/borgbackup-fuse.rb
@@ -1,29 +1,5 @@
 require "digest"
 
-class OsxfuseRequirement < Requirement
-  fatal true
-
-  satisfy(build_env: false) { self.class.binary_osxfuse_installed? }
-
-  def self.binary_osxfuse_installed?
-    File.exist?("/usr/local/include/osxfuse/fuse.h") &&
-      !File.symlink?("/usr/local/include/osxfuse")
-  end
-
-  env do
-    ENV.append_path "PKG_CONFIG_PATH", HOMEBREW_LIBRARY/"Homebrew/os/mac/pkgconfig/fuse"
-
-    unless HOMEBREW_PREFIX.to_s == "/usr/local"
-      ENV.append_path "HOMEBREW_LIBRARY_PATHS", "/usr/local/lib"
-      ENV.append_path "HOMEBREW_INCLUDE_PATHS", "/usr/local/include/osxfuse"
-    end
-  end
-
-  def message
-    "osxfuse is required to build borgbackup-fuse. Please run `brew install --cask osxfuse` first."
-  end
-end
-
 class BorgbackupFuse < Formula
   include Language::Python::Virtualenv
 
