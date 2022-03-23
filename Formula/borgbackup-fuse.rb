9a6a152e95803fe6f3335daebbef2bcce1c867e8
Manu <manu@snapdragon.cc>
2022-03-23 12:27:43 +0400

    Upgrade to Borg 1.2.0

--- Formula/borgbackup-fuse.rb
+++ Formula/borgbackup-fuse.rb
@@ -32,8 +32,8 @@ class BorgbackupFuse < Formula
 
   desc "Deduplicating archiver with compression and authenticated encryption"
   homepage "https://borgbackup.org/"
-  url "https://github.com/borgbackup/borg/releases/download/1.1.17/borgbackup-1.1.17.tar.gz"
-  sha256 "7ab924fc017b24929bedceba0dcce16d56f9868bf9b5050d2aae2eb080671674"
+  url "https://files.pythonhosted.org/packages/0f/e9/0b07aaff39e22eee9a983a48507bff46cf0d634e8a24e635e336cd7b3bce/borgbackup-1.2.0.tar.gz"
+  sha256 "e39a5547902ef456101aa4c779fa66b345bda70d16788e8bd18e458f93af7f67"
   license "BSD-3-Clause"
 
   livecheck do
@@ -46,22 +46,38 @@ class BorgbackupFuse < Formula
   depends_on "libb2"
   depends_on "lz4"
   depends_on "openssl@1.1"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
+  depends_on "xxhash"
   depends_on "zstd"
 
   conflicts_with "borgbackup", because: "borgbackup-fuse is a patched version of borgbackup"
+
+  resource "msgpack" do
+    url "https://files.pythonhosted.org/packages/61/3c/2206f39880d38ca7ad8ac1b28d2d5ca81632d163b2d68ef90e46409ca057/msgpack-1.0.3.tar.gz"
+    sha256 "51fdc7fb93615286428ee7758cecc2f374d5ff363bdd884c7ea622a7a327a81e"
+  end
+
+  resource "packaging" do
+    url "https://files.pythonhosted.org/packages/df/9e/d1a7217f69310c1db8fdf8ab396229f55a699ce34a203691794c5d1cad0c/packaging-21.3.tar.gz"
+    sha256 "dd47c42927d89ab911e606518907cc2d3a1f38bbd026385970643f9c5b8ecfeb"
+  end
+
+  resource "pyparsing" do
+    url "https://files.pythonhosted.org/packages/d6/60/9bed18f43275b34198eb9720d4c1238c68b3755620d20df0afd89424d32b/pyparsing-3.0.7.tar.gz"
+    sha256 "18ee9022775d270c55187733956460083db60b37d0d0fb357445f3094eed3eea"
+  end
 
   resource "llfuse" do
     url "https://files.pythonhosted.org/packages/b1/d4/44443fbaac6d5b878da99e7c0948ee93c7934fa3b00e48c5363823b583d0/llfuse-1.4.1.tar.gz"
     sha256 "c29c79d96a5aeab51608cae12594a1bf83576d86232f97341c7f779d413a4ec9"
   end
 
-  resource "packaging" do
-    url "https://files.pythonhosted.org/packages/df/86/aef78bab3afd461faecf9955a6501c4999933a48394e90f03cd512aad844/packaging-21.0.tar.gz"
-    sha256 "7dc96269f53a4ccec5c0670940a4281106dd0bb343f47b7471f779df49c2fbe7"
-  end
-
   def install
+    ENV["BORG_LIBB2_PREFIX"] = Formula["libb2"].prefix
+    ENV["BORG_LIBLZ4_PREFIX"] = Formula["lz4"].prefix
+    ENV["BORG_LIBXXHASH_PREFIX"] = Formula["xxhash"].prefix
+    ENV["BORG_LIBZSTD_PREFIX"] = Formula["zstd"].prefix
+    ENV["BORG_OPENSSL_PREFIX"] = Formula["openssl@1.1"].prefix
     virtualenv_install_with_resources
   end
 
