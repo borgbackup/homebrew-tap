d99b74e111eca805ecedbe1baebf94e02854138d
sMtzDczDq <75165692+sMtzDczDq@users.noreply.github.com>
2024-07-08 17:09:41 +0200

    Update borgbackup-fuse.rb

--- Formula/borgbackup-fuse.rb
+++ Formula/borgbackup-fuse.rb
@@ -71,7 +71,7 @@ class BorgbackupFuse < Formula
   resource "packaging" do
     url "https://files.pythonhosted.org/packages/51/65/50db4dda066951078f0a96cf12f4b9ada6e4b811516bf0262c0f4f7064d4/packaging-24.1.tar.gz"
     sha256 "026ed72c8ed3fcce5bf8950572258698927fd1dbda10a5e981cdf0ac37f4f002"
-    end
+  end
 
   def install
     ENV["BORG_LIBB2_PREFIX"] = Formula["libb2"].prefix
